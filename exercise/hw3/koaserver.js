a471b61a08609c2342bdce84ab3870498e4a44ce
zxc22273146 <zxc22273146@gmail.com>
2018-09-28 11:05:33 +0800

    20180928

--- exercise/hw3/koaserver.js
+++ exercise/hw3/koaserver.js
@@ -3,16 +3,16 @@ const app = module.exports = new Koa();
 
 app.use(async function(ctx) {
   if(ctx.url == "/hello"){
-    console.log(ctx.url);
     ctx.body = "你好";
+    console.log(ctx.url);
   }
   else if(ctx.url == "/name"){
-    console.log(ctx.url);
     ctx.body = "馮志揚";
+    console.log(ctx.url);
   }
   else if(ctx.url == "/id"){
-    console.log(ctx.url);
     ctx.body = "110510524";
+    console.log(ctx.url);
   }
   else
     ctx.status = 404;
